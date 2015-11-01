8c55109d37b12a7f3d3d32055bb93dec0f30e834
Sir Tidez <iTidez@cobaltstudios.org>
2015-11-01 01:39:27 -0700

    Implemented basic plugin features and registered it as a Spectrum Plugin

--- src/main/java/net/spectrumnation/CoreController.java
+++ src/main/java/net/spectrumnation/CoreController.java
@@ -1,5 +1,6 @@
 package net.spectrumnation;
 
+import java.util.HashMap;
 import java.util.logging.Logger;
 
 /**
@@ -9,10 +10,12 @@ public class CoreController {
     private static boolean isInitialized = false;
     private static Logger logger;
     private static SpectrumCore corePlugin;
+    private static HashMap<String, SpectrumPlugin> registeredListeners;
 
     public void init(SpectrumCore core) {
         corePlugin = core;
         logger = Logger.getLogger("Minecraft");
+        registeredListeners = new HashMap<String, SpectrumPlugin>();
         isInitialized = true;
     }
 
@@ -32,5 +35,13 @@ public class CoreController {
 
     public static SpectrumCore corePlugin() {
         return corePlugin;
+    }
+
+    public static boolean registerPlugin(String name, SpectrumPlugin pluginClass) {
+        if(registeredListeners.containsKey(name) || registeredListeners.containsValue(pluginClass))     return false;
+        else {
+            registeredListeners.put(name, pluginClass);
+            return true;
+        }
     }
 }
---
+++ src/main/java/net/spectrumnation/SpectrumPlugin.java
@@ -0,0 +1,6 @@
+package net.spectrumnation;
+
+/**
+ * Created by sirtidez on 11/1/15.
+ */
+public interface SpectrumPlugin {}
